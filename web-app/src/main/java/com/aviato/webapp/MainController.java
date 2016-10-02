3fd02dc1c87dd3faadb2bc096492aafdd747cfdc
Amal Nazeem <amalnazeem@gmail.com>
2016-10-01 20:07:15 -0400

    UserLogin??

--- web-app/src/main/java/com/aviato/webapp/MainController.java
+++ web-app/src/main/java/com/aviato/webapp/MainController.java
@@ -2,16 +2,25 @@ package com.aviato.webapp;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.scope.ScopeBuilder;
+
 @Controller
 public class MainController {
+    
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public ModelAndView home(Locale locale) {
@@ -50,5 +59,21 @@ public class MainController {
 		mv.addObject("serverTime", formattedDate.toString());
 		mv.setViewName("about");
 		return mv;
+	}
+	
+	@RequestMapping(value = "/fbtest", method = RequestMethod.GET)
+	public ModelAndView fbTest(Locale locale) {	    
+	    ModelAndView mv = new ModelAndView();
+	    
+	    ScopeBuilder scopeBuilder = new ScopeBuilder();
+	    
+	    FacebookClient client = new DefaultFacebookClient(Version.LATEST);
+	    
+	    
+	    
+        mv.setViewName("fbTest");
+	    System.out.println("Navigating to FBTest");
+	    mv.setViewName("fbTest");
+	    return mv;
 	}
 }
