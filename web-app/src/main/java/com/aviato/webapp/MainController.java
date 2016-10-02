60d1fcdbb7f4608f99ef4e40da442fb431758deb
Amal Nazeem <amalnazeem@gmail.com>
2016-10-01 22:52:58 -0400

    Stuff is Wokring???

--- web-app/src/main/java/com/aviato/webapp/MainController.java
+++ web-app/src/main/java/com/aviato/webapp/MainController.java
@@ -7,14 +7,19 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
+import com.restfb.FacebookClient.AccessToken;
+import com.restfb.Parameter;
 import com.restfb.Version;
 import com.restfb.scope.ScopeBuilder;
 
@@ -62,18 +67,37 @@ public class MainController {
 	}
 	
 	@RequestMapping(value = "/fbtest", method = RequestMethod.GET)
-	public ModelAndView fbTest(Locale locale) {	    
-	    ModelAndView mv = new ModelAndView();
+	public ModelAndView fbTest(Locale locale) {        
+        ModelAndView mv = new ModelAndView();
 	    
 	    ScopeBuilder scopeBuilder = new ScopeBuilder();
 	    
 	    FacebookClient client = new DefaultFacebookClient(Version.LATEST);
 	    
+	    String loginUrl = client.getLoginDialogUrl("1305051219545441", "http://localhost:4997/fbloggedin", scopeBuilder,
+	            Parameter.with("response_type", "code"));
 	    
-	    
-        mv.setViewName("fbTest");
+	    return new ModelAndView("redirect:" +loginUrl);
+	    /*
 	    System.out.println("Navigating to FBTest");
 	    mv.setViewName("fbTest");
 	    return mv;
+	    */
+	    
+	}
+	
+	@RequestMapping(value = "/fbloggedin", method = RequestMethod.GET)
+	public ModelAndView fbLoggedIn(
+	        @RequestParam("code") String code) {
+	    FacebookClient client = new DefaultFacebookClient(Version.LATEST);
+	    
+	    AccessToken token = client.obtainUserAccessToken("1305051219545441", "caecfb1894f0f6c80c5220cb86a55a7c", 
+	            "http://localhost:4997/fbloggedin", code);
+	    
+	    System.out.println("Expires: " +token.getExpires().toString());
+	    System.out.println("Actual Token: " +token.getAccessToken());
+	    System.out.println("Token Type: " +token.getTokenType());
+	    
+	    return null;
 	}
 }
